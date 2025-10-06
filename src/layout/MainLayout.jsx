da83a8049a2d37da5bc6442cecaa1e4678526b48
islamulhaque <islamulbd24@gmail.com>
2025-10-06 13:04:19 +0600

    update vercel.json file

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Header from "../components/header/header";
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 
 export default function MainLayout() {
   return (
